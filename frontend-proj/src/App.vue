9b8463132ddcfc2b45d9cf24cb18159620d0ce5c
Jakub Adamski <kuba@exemplum.pl>
2020-06-17 12:35:33 +0200

    homepage #26

--- frontend-proj/src/App.vue
+++ frontend-proj/src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app">
-    <div class="menu">
+    <div class="menu" :class="$mq">
       <router-link :to="{ name: 'home' }" class="home_b" :class="$mq">&#9903;</router-link>
-      <router-link :to="{ name: 'creator' }" class="creator_b" :class="$mq">+</router-link>
       <router-link :to="{ name: 'settings' }" class="login_b" :class="$mq">
-        <div v-if="!$store.state.jwt">&#9868;</div>
-        <div v-else style="margin-top: 3px"> &#9881; </div>
+        <div class="get-started" :class="$mq" v-if="!$store.state.jwt">Get started</div>
+        <div v-else style="margin-top: 3px"> &#9868; </div>
       </router-link>
     </div>
     <router-view></router-view>
@@ -50,7 +49,7 @@ export default {
 
 <style lang="sass">
 html
-    scroll-padding-top: 60px
+    scroll-padding-top: 85px
 
 @font-face
     font-family: 'Raleway-Regular'
@@ -59,6 +58,14 @@ html
 @font-face 
     font-family: 'OpenSans-Regular'
     src: url('./assets/fonts/OpenSans/OpenSans-Regular.ttf')
+
+.get-started
+    background: #B8352D
+    padding: 3px 20px
+    border-radius: 5px
+    font-size: 16px
+    &.small
+        font-size: 14px
 
 #app 
     -webkit-font-smoothing: antialiased
@@ -72,41 +79,31 @@ html
     z-index: 4
     color: white
     float: right
-    margin: 7px
-    margin-right: 12px
+    margin: 18px 30px
     font-size: 23px
     &.small
-        margin-right: 2px
+        margin-right: 5%
+
 
 .home_b
     z-index: 4
     color: white
     text-decoration: none
     float: left
-    margin: 11px
-    font-size: 25px
-    margin-left: 30px
-
-.creator_b
-    z-index: 4
-    color: white
-    padding: 0 9px
-    border-radius: 5px
-    font-size: 30px
-    float: right
-    margin: 7px
-    margin-right: 30px
-    text-decoration: none
+    margin: 9px 30px
+    font-size: 35px
+    letter-spacing: 3px
     font-family: Raleway-Regular
     &.small
-        margin-right: 5px
+        margin-left: 1px
+        transform: rotate(90deg)
 
 .menu
     z-index: 3
     position: fixed
     top: 0
     background: #303030
-    height: 50px
+    height: 60px
     width: 100%
 
 body
--- frontend-proj/src/components/Home.vue
+++ frontend-proj/src/components/Home.vue
@@ -1,39 +1,39 @@
 <template>
 <div>
-    <form action="javascript:void(0);" class="search">
-        <input type="search" onfocus="window.scrollTo({ top: 0, behavior: 'smooth' })" id="search-input">
-        <input type="submit" v-on:click="search()" class="search-b" value="Search">
+    <form action="javascript:void(0);" class="search" :class="$mq">
+        <input placeholder="Search" type="search" class="search-input" onfocus="window.scrollTo({ top: 0, behavior: 'smooth' })" id="search-input">
+        <button type="submit" v-on:click="search()" class="search-b" :class="$mq">&#9906;</button>
     </form>
     <div id="homepage" :class="$mq">
         <div v-if="searchResults.length == 0">
-            <div class="element" v-for="(timeline, idx) in $store.state.timelines" :key="idx"> 
-                <div class="s_line"></div>
-                <div class="category">{{ timeline.category }}</div>
-                <router-link :to="{ path: 'timeline/' + timeline.id }" class="image_container" v-if="timeline.pictures.length > 0">
-                    <img class="image" :src="timeline.pictures[0]">
+            <div class="element" v-for="(timeline, idx) in $store.state.timelines" :key="idx" :class="$mq"> 
+                <div class="s_line" :class="$mq"></div>
+                <div class="category" :class="$mq">{{ timeline.category }}</div>
+                <router-link :to="{ path: 'timeline/' + timeline.id }" class="title" :class="$mq">{{ timeline.descriptionTitle }}</router-link>
+                <router-link :to="{ path: 'timeline/' + timeline.id }" class="desc" :class="$mq">
+                    {{ timeline.description.substring(0, 100) }}...
+                    <div class="author">by {{ timeline.user.username }} <div class="views">&#x2687; {{ timeline.views }} &#9825; {{ timeline.likes }} </div></div>
                 </router-link>
-                <router-link :to="{ path: 'timeline/' + timeline.id }" class="title">{{ timeline.descriptionTitle }}</router-link>
-                <router-link :to="{ path: 'timeline/' + timeline.id }" class="desc">
-                    {{ timeline.description.substring(0, 250) }}...
+                 <router-link :to="{ path: 'timeline/' + timeline.id }" class="image_container" :class="$mq" v-if="timeline.pictures.length > 0">
+                    <img :class="$mq" class="image" :src="timeline.pictures[0]">
                 </router-link>
-                <div class="author">by {{ timeline.user.username }}, <div class="views">{{ timeline.views }} views, {{ timeline.likes }} likes</div></div>
             </div>
         </div>
         <div v-else>
-            <h1>Search results:</h1>
-            <div v-on:click="quit()" class="quit">x</div>
-            <div class="element" v-for="(timeline, idx) in searchResults" :key="idx">
+            <h1 :class="$mq">Search results:</h1>
+            <div v-on:click="quit()" class="quit" :class="$mq">x</div>
+            <div class="element" v-for="(timeline, idx) in searchResults" :key="idx" :class="$mq">
                 <div v-if="!timeline.none">
-                    <div class="s_line"></div>
-                    <div class="category"></div>
-                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="image_container" v-if="timeline.pictures.length > 0">
-                        <img class="image" :src="timeline.pictures[0]">
-                    </router-link>
-                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="title">{{ timeline.descriptionTitle }}</router-link>
-                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="desc">
+                    <div class="s_line" :class="$mq"></div>
+                    <div class="category" :class="$mq"></div>
+                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="title" :class="$mq">{{ timeline.descriptionTitle }}</router-link>
+                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="desc" :class="$mq">
                         {{ timeline.description.substring(0, 250) }}...
+                        <div class="author">by {{ timeline.user.username }} <div class="views">{{ timeline.views }} views, {{ timeline.likes }} &#9825;</div></div>
                     </router-link>
-                    <div class="author">by {{ timeline.user.username }}, <div class="views">{{ timeline.views }} views, {{ timeline.likes }} likes</div></div>
+                    <router-link :to="{ path: 'timeline/' + timeline.id }" class="image_container" :class="$mq" v-if="timeline.pictures.length > 0">
+                        <img :class="$mq" class="image" :src="timeline.pictures[0]">
+                    </router-link>
                 </div>
                 <div v-else class="empty">Can't find.</div>
             </div>
@@ -80,21 +80,23 @@ export default {
             })
         },
         search(){
-            var timelinesApi = this.baseApi + 'timelines/public/search'
-            this.axios.get(timelinesApi, {
-                params: {
-                    text: document.getElementById("search-input").value
-                }
-            })
-            .then(response => {
-                this.searchResults = response.data
-                if (response.data.length == 0){
-                    this.searchResults = [{none: true}]
-                }
-            })
-            .catch(error => {
-                console.log(error)
-            })
+            if (document.getElementById("search-input").value){
+                var timelinesApi = this.baseApi + 'timelines/public/search'
+                this.axios.get(timelinesApi, {
+                    params: {
+                        text: document.getElementById("search-input").value
+                    }
+                })
+                .then(response => {
+                    this.searchResults = response.data
+                    if (response.data.length == 0){
+                        this.searchResults = [{none: true}]
+                    }
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            } 
         },
         quit(){
             document.getElementById("search-input").value = ''
@@ -115,6 +117,7 @@ export default {
     padding-top: 50px
     text-align: left
     font-size: 20px
+    margin-left: 5%
 
 .quit
     position: absolute
@@ -122,25 +125,62 @@ export default {
     top: 100px
     font-family: OpenSans-Regular
     font-size: 30px
+    color: #7e7e7e
+    &.small
+        right: 5%
+    &.medium
+        right: 10%
 
 h1
     font-family: Raleway-Regular
     font-size: 40px
     text-align: left
-    margin-left: 18%
+    margin-left: 20%
     margin-top: 50px
+    &.medium
+        margin-left: 10%
+    &.small
+        margin-left: 5%
+
+.search-input
+    background: #303030
+    padding: 0 10px
+    outline: none
+    border: none
+    color: white
+    width: 85%
+    height: 30px
+    border-radius: 5px
+    font-size: 17px
 
 .search-b
     display: inline-block
-    margin-left: 20px
+    margin-left: 5%
+    background: none
+    border: none
+    outline: none
+    color: #b8b8b8
+    width: 10%
+    font-size: 25px
+    transform: translateY(+4px) translateX(-10px)rotate(-45deg)
+    &.small
+        transform: translateY(+8px) translateX(-10px)rotate(-45deg)
+
+input::-webkit-search-cancel-button
+  -webkit-appearance: none
 
 .search
+    width: 25%
+    font-family: OpenSans-Regular
     position: fixed
     z-index: 4
-    width: 20%
-    transform: translateY(-35px) translateX(-50%)
+    transform: translateY(-38px) translateX(-50%)
     left: 50%
-    color: white
+    &.medium
+        width: 35%
+    &.small
+        width: 40%
+        transform: translateY(-38px) translateX(-91%)
 
 .views
     margin-top: 5px
@@ -149,8 +189,9 @@ h1
     font-family: OpenSans-Regular
     margin-top: 30px
     text-align: left
-    color: #7e7e7e
     font-size: 16px
+    color: #7e7e7e
+    margin-left: 5%
 
 .author
     font-weight: normal
@@ -161,32 +202,51 @@ h1
     color: #7e7e7e
 
 .s_line
-    width: calc( 100% + 100px )
-    transform: translateX(-50px)
+    width: 100%
     height: 70px
     background: white
     display: inline-block
+    &.small
+        height: 40px
 
 .image_container
-    position: absolute
-    display: block
-    right: 20%
-    margin-top: 5px
+    display: inline-block
+    width: 21%
+    margin-left: 6%
+    margin-right: 3%
+    transform: translateY(-40px)
+    &.medium
+        width: 29%
+    &.small
+        width: 90%
+        margin: 30px 0
+        margin-left: 5%
+        transform: translateY(0)
+        display: block
 
 .image
     border-radius: 1px
     height: 170px
-    width: 260px
+    width: 100%
     object-fit: cover
+    &.medium
+        height: 150px
 
 .desc
     text-decoration: none
     color: #303030
     margin-top: 20px
     font-family: OpenSans-Regular
-    display: block
-    width: 73%
+    display: inline-block
+    vertical-align: top
+    width: 65%
     text-align: justify 
+    margin-left: 5%
+    &.medium
+        width: 57%
+    &.small
+        width: 90%
+        display: block
 
 .title
     text-decoration: none
@@ -198,15 +258,23 @@ h1
     font-size: 40px
     font-weight: bold
     letter-spacing: 1px
+    margin-left: 5%
 
 
 .element
     border-radius: 5px
-    padding: 0 50px
     padding-bottom: 60px
     width: 60%
-    margin-left: calc(20% - 50px)
+    margin-left: 20%
     background: $bg-color
+    &.medium
+        width: 80%
+        margin-left: 10%
+    &.small
+        width: 100%
+        margin-left: 0
+        
+        padding-bottom: 60px
 
 #homepage
     padding-top: 1px
